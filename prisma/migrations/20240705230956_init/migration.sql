8881808c7b573c89ac363f7043598e855f2c3a16
Wasiu Bakare <wasiubakare@gmail.com>
2024-07-06 00:45:32 +0100

    Add database migrations

---
+++ prisma/migrations/20240705230956_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "userId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "organisations" (
+    "orgId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "organisations_pkey" PRIMARY KEY ("orgId")
+);
+
+-- CreateTable
+CREATE TABLE "_OrganisationToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OrganisationToUser_AB_unique" ON "_OrganisationToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OrganisationToUser_B_index" ON "_OrganisationToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_OrganisationToUser" ADD CONSTRAINT "_OrganisationToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "organisations"("orgId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OrganisationToUser" ADD CONSTRAINT "_OrganisationToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
